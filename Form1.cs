4b45e6266c590f135eda2d201b075661942ec7ea
Tien Lam Nguyen <tien.lam.130293@gmail.com>
2021-05-01 23:07:05 +0700

    Fix 1

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace PM2220
     public partial class Form1 : Form       // Khai báo class Form1 thuộc Class Form
     {
         ModbusClient modbusClient; // Khai báo trường (Field) modbusClient thuộc Class ModbusClient
-        //int[] Value = { 0};
+        
         PointPairList point1;
         PointPairList point2;
         PointPairList point3;
@@ -25,6 +25,15 @@ namespace PM2220
         float Voltage1;
         float Voltage2;
         float Voltage3;
+        float Current1;
+        float Current2;
+        float Current3;
+        float Freq;
+        float ActiveP; 
+        float ReActiveP;
+        float PFactor;
+        float Energy;
+        float CurrentUnbalance;
         int i = 2;
         public Form1()
         {
@@ -35,22 +44,20 @@ namespace PM2220
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            //var listPort = new[] { "COM1", "COM2", "COM3", "COM4", "COM5" }; 
+            
             comboBox1.DataSource = SerialPort.GetPortNames(); //tạo list cổng COM cho combobox
 
+            /* Đoạn code này để khởi tạo đồ thị dùng thư viện ZedGraph*/
             GraphPane graphPane = zedGraphControl1.GraphPane;
             graphPane.Title.Text = "Voltage";
             graphPane.XAxis.Title.Text = "Thời gian";
             graphPane.YAxis.Title.Text = "Voltage";
-
             graphPane.XAxis.Type = AxisType.Date;
-            graphPane.XAxis.Scale.Format = "hh:mm:ss";
-            //graphPane.XAxis.Scale.Max = 30;
-            graphPane.XAxis.Scale.MinorStep = 1;
+            graphPane.XAxis.Scale.Format = "hh:mm:ss";       
+            graphPane.XAxis.Scale.MinorStep = 5;
             graphPane.XAxis.Scale.MinorUnit = DateUnit.Second;
-
             graphPane.XAxis.Scale.MajorUnit = DateUnit.Second;
-            graphPane.XAxis.Scale.MajorStep = 5;
+            graphPane.XAxis.Scale.MajorStep = 30;
             graphPane.YAxis.Scale.Min = 0;
             graphPane.YAxis.Scale.Max = 260;
             point1 = new PointPairList();
@@ -62,10 +69,7 @@ namespace PM2220
             LineItem myCurve2 = graphPane.AddCurve("L2Voltage", point2, System.Drawing.Color.Blue);
 
             LineItem myCurve3 = graphPane.AddCurve("L3Voltage", point3, System.Drawing.Color.Yellow);
-            //points.Add(new XDate(DateTime.Now), 100);
-            //points.Add(new XDate(DateTime.Now), 200);
-            // points.Add(new XDate(DateTime.Now), 300);
-            // graphPane.AxisChange();
+            
 
         }
 
@@ -75,6 +79,7 @@ namespace PM2220
         private void button1_Click(object sender, EventArgs e) // Method kết nối với thiết bị  khi click vào
         {
             modbusClient = new ModbusClient(); // Khai bao ket noi voi modbus client tai cong COM4
+
             //Khai bao cac thuoc tinh cua cong COM
             modbusClient.SerialPort = comboBox1.SelectedItem.ToString();
             modbusClient.Baudrate = 19200;
@@ -92,9 +97,9 @@ namespace PM2220
             if (modbusClient.Connected)
             {
                 MessageBox.Show("Kết nối thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                //textBox4.Text = ("Connected");
+                textBox4.Text = "Connected";
             }
-            textBox4.Text = modbusClient.Connected.ToString();
+           else { textBox4.Text = "Error"; }
         }
 
         private void ReadValue() // Đọc giá trị dòng và điện áp theo địa chỉ thanh ghi
@@ -103,13 +108,33 @@ namespace PM2220
             {
                 while (modbusClient.Connected)
                 {
+                    // Đọc giá trị điện áp theo địa chỉ thanh ghi theo truyền thông Modbus và thư viện easyModbus
                     Voltage1 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3027, 2), ModbusClient.RegisterOrder.HighLow);
                     Voltage2 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3029, 2), ModbusClient.RegisterOrder.HighLow);
                     Voltage3 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3031, 2), ModbusClient.RegisterOrder.HighLow);
-                    //Value = modbusClient.ReadHoldingRegisters(0, 15);
+                    Current1 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(2999, 2), ModbusClient.RegisterOrder.HighLow);
+                    Current2 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3001, 2), ModbusClient.RegisterOrder.HighLow);
+                    Current3 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3003, 2), ModbusClient.RegisterOrder.HighLow);
+                    Freq = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3009, 2), ModbusClient.RegisterOrder.HighLow);
+                    ActiveP = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3059, 2), ModbusClient.RegisterOrder.HighLow);
+                    ReActiveP = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3067, 2), ModbusClient.RegisterOrder.HighLow);
+                    PFactor = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3083, 2), ModbusClient.RegisterOrder.HighLow);
+                    //Energy = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3067, 2), ModbusClient.RegisterOrder.HighLow);
+                    CurrentUnbalance = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3017, 2), ModbusClient.RegisterOrder.HighLow);
+
+                    // Hiển thị giá trị điện áp lên textbox theo thời gian thực bằng backgroundworker và beginInvoke
                     textBox1.BeginInvoke(new Action(() => { textBox1.Text = Voltage1.ToString(); }));
                     textBox2.BeginInvoke(new Action(() => { textBox2.Text = Voltage2.ToString(); }));
                     textBox3.BeginInvoke(new Action(() => { textBox3.Text = Voltage3.ToString(); }));
+                    textBox5.BeginInvoke(new Action(() => { textBox5.Text = Current1.ToString(); }));
+                    textBox6.BeginInvoke(new Action(() => { textBox6.Text = Current2.ToString(); }));
+                    textBox7.BeginInvoke(new Action(() => { textBox7.Text = Current3.ToString(); }));
+                    textBox8.BeginInvoke(new Action(() => { textBox8.Text = Freq.ToString(); }));
+                    textBox9.BeginInvoke(new Action(() => { textBox9.Text = ActiveP.ToString(); }));
+                    textBox10.BeginInvoke(new Action(() => { textBox10.Text = ReActiveP.ToString(); }));
+                    textBox11.BeginInvoke(new Action(() => { textBox11.Text = PFactor.ToString(); }));
+                    textBox12.BeginInvoke(new Action(() => { textBox12.Text = Energy.ToString(); }));
+                    textBox13.BeginInvoke(new Action(() => { textBox13.Text = CurrentUnbalance.ToString(); }));
                 }
             }
             catch (Exception)
@@ -120,63 +145,34 @@ namespace PM2220
         }
 
 
-        private void button3_Click(object sender, EventArgs e)
+        // cho phép backgroundWorker hoạt động
+        private void button3_Click(object sender, EventArgs e) 
         {
             if (backgroundWorker1.IsBusy != true)
             {
                 backgroundWorker1.RunWorkerAsync();
             }
 
-
-            /*
-           while (modbusClient.Connected)
-            {
-                try 
-                {
-                    ReadValue();
-                    //System.Threading.Thread.Sleep(2000);
-                }
-                catch (ArgumentException ex)
-                {
-                    MessageBox.Show("Địa chỉ thanh ghi không chính xác. \n" + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                catch (NullReferenceException)
-                {
-                    MessageBox.Show("Vui lòng kiểm tra lại kết nối với thiết bị", "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-           */
         }
 
+
+        //backgroundWorker thực hiện hàm Readvalue
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             ReadValue();
-        }
+        } 
 
+
+        // Khởi động timer1
         private void button2_Click(object sender, EventArgs e)
         {
-            /*
-            while (true)
-            {
-                float Voltage1 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3019, 2), ModbusClient.RegisterOrder.HighLow);
-                float Voltage2 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3021, 2), ModbusClient.RegisterOrder.HighLow);
-                float Voltage3 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3023, 2), ModbusClient.RegisterOrder.HighLow);
-            
-                
-            }
-            */
-            //Value = modbusClient.ReadHoldingRegisters(0, 15);
-            //System.Threading.Thread.Sleep(2000);
-            // textBox1.Text = Value[0].ToString();
-
-            //textBox2.Text = Value[0].ToString();
-            //textBox3.Text = Value[3].ToString();
-            timer1.Start(); // set timer1.Enabled = true
-                            // timer1.Enabled = true;
-
+      
+            timer1.Start(); 
 
         }
 
+
+        // Hàm khởi tạo file exel
         private void Exel()
         {
             xla = new Microsoft.Office.Interop.Excel.Application();
@@ -194,10 +190,20 @@ namespace PM2220
             ws.Columns.AutoFit();
             wb.SaveAs("C:\\Users\\os\\Desktop\\New folder (3)\\test.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
         }
+
+
+        // chạy hàm Exel
         private void button4_Click(object sender, EventArgs e)
         {
+            
             Exel();
+            //timer1.Start();
+          //  SavetoExel();  
+
         }
+
+
+        //hàm lưu giá trị đo vào file exel  
         private void SavetoExel()
         {
 
@@ -208,7 +214,9 @@ namespace PM2220
 
             i++;
         }
-        private void timer1_Tick(object sender, EventArgs e) // vẽ đồ thị theo khoảng thời gian Timer 1
+
+        // Update đồ thị và exel theo Timer 1
+        private void timer1_Tick(object sender, EventArgs e) 
         {
 
             point1.Add(new XDate(DateTime.Now), Voltage1);
@@ -219,6 +227,8 @@ namespace PM2220
             SavetoExel();
         }
 
+        // Hiển thị file Exel
+        
         private void button5_Click(object sender, EventArgs e)
         {
 
@@ -226,9 +236,6 @@ namespace PM2220
 
         }
 
-        private void zedGraphControl1_Load(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
