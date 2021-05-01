d62c18b802096a2d36c648a932ca35a394d55d85
Lam_Thinkpad <tien.lam.130293@gmail.com>
2021-05-01 17:59:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace PM2220
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(214, 137);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(557, 276);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.Load += new System.EventHandler(this.zedGraphControl1_Load);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 290);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(51, 342);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(71, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "VeDothi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Hienthigiatri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "CreatExel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(106, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "ViewExel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(311, 15);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using EasyModbus;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.IO.Ports;
+using ZedGraph;
+
+
+namespace PM2220
+{
+
+    public partial class Form1 : Form       // Khai báo class Form1 thuộc Class Form
+    {
+        ModbusClient modbusClient; // Khai báo trường (Field) modbusClient thuộc Class ModbusClient
+        //int[] Value = { 0};
+        PointPairList point1;
+        PointPairList point2;
+        PointPairList point3;
+
+        Microsoft.Office.Interop.Excel.Application xla;
+
+        Microsoft.Office.Interop.Excel._Workbook wb;
+        Microsoft.Office.Interop.Excel.Worksheet ws;
+        Microsoft.Office.Interop.Excel.Range range;
+        float Voltage1;
+        float Voltage2;
+        float Voltage3;
+        int i = 2;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            //var listPort = new[] { "COM1", "COM2", "COM3", "COM4", "COM5" }; 
+            comboBox1.DataSource = SerialPort.GetPortNames(); //tạo list cổng COM cho combobox
+
+            GraphPane graphPane = zedGraphControl1.GraphPane;
+            graphPane.Title.Text = "Voltage";
+            graphPane.XAxis.Title.Text = "Thời gian";
+            graphPane.YAxis.Title.Text = "Voltage";
+
+            graphPane.XAxis.Type = AxisType.Date;
+            graphPane.XAxis.Scale.Format = "hh:mm:ss";
+            //graphPane.XAxis.Scale.Max = 30;
+            graphPane.XAxis.Scale.MinorStep = 1;
+            graphPane.XAxis.Scale.MinorUnit = DateUnit.Second;
+
+            graphPane.XAxis.Scale.MajorUnit = DateUnit.Second;
+            graphPane.XAxis.Scale.MajorStep = 5;
+            graphPane.YAxis.Scale.Min = 0;
+            graphPane.YAxis.Scale.Max = 260;
+            point1 = new PointPairList();
+            point2 = new PointPairList();
+            point3 = new PointPairList();
+
+            LineItem myCurve1 = graphPane.AddCurve("L1Voltage", point1, System.Drawing.Color.Red);
+
+            LineItem myCurve2 = graphPane.AddCurve("L2Voltage", point2, System.Drawing.Color.Blue);
+
+            LineItem myCurve3 = graphPane.AddCurve("L3Voltage", point3, System.Drawing.Color.Yellow);
+            //points.Add(new XDate(DateTime.Now), 100);
+            //points.Add(new XDate(DateTime.Now), 200);
+            // points.Add(new XDate(DateTime.Now), 300);
+            // graphPane.AxisChange();
+
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e) // Method kết nối với thiết bị  khi click vào
+        {
+            modbusClient = new ModbusClient(); // Khai bao ket noi voi modbus client tai cong COM4
+            //Khai bao cac thuoc tinh cua cong COM
+            modbusClient.SerialPort = comboBox1.SelectedItem.ToString();
+            modbusClient.Baudrate = 19200;
+            modbusClient.Parity = Parity.None;
+            modbusClient.StopBits = StopBits.One;
+
+            // Mở kết nối với ModbusClient
+            try { modbusClient.Connect(); }
+            catch (Exception)
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại cổng COM và cài đặt thiết bị: \n \n SlaveID = 1 \n Baudrate = 19200 \n Parity = None \n Stopbits = One",
+                    "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (modbusClient.Connected)
+            {
+                MessageBox.Show("Kết nối thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //textBox4.Text = ("Connected");
+            }
+            textBox4.Text = modbusClient.Connected.ToString();
+        }
+
+        private void ReadValue() // Đọc giá trị dòng và điện áp theo địa chỉ thanh ghi
+        {
+            try
+            {
+                while (modbusClient.Connected)
+                {
+                    Voltage1 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3027, 2), ModbusClient.RegisterOrder.HighLow);
+                    Voltage2 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3029, 2), ModbusClient.RegisterOrder.HighLow);
+                    Voltage3 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3031, 2), ModbusClient.RegisterOrder.HighLow);
+                    //Value = modbusClient.ReadHoldingRegisters(0, 15);
+                    textBox1.BeginInvoke(new Action(() => { textBox1.Text = Voltage1.ToString(); }));
+                    textBox2.BeginInvoke(new Action(() => { textBox2.Text = Voltage2.ToString(); }));
+                    textBox3.BeginInvoke(new Action(() => { textBox3.Text = Voltage3.ToString(); }));
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Vui long kiểm tra kết nối với thiết bị", "lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+
+
+            /*
+           while (modbusClient.Connected)
+            {
+                try 
+                {
+                    ReadValue();
+                    //System.Threading.Thread.Sleep(2000);
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show("Địa chỉ thanh ghi không chính xác. \n" + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (NullReferenceException)
+                {
+                    MessageBox.Show("Vui lòng kiểm tra lại kết nối với thiết bị", "Lỗi kết nối", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+           */
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ReadValue();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+            while (true)
+            {
+                float Voltage1 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3019, 2), ModbusClient.RegisterOrder.HighLow);
+                float Voltage2 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3021, 2), ModbusClient.RegisterOrder.HighLow);
+                float Voltage3 = ModbusClient.ConvertRegistersToFloat(modbusClient.ReadHoldingRegisters(3023, 2), ModbusClient.RegisterOrder.HighLow);
+            
+                
+            }
+            */
+            //Value = modbusClient.ReadHoldingRegisters(0, 15);
+            //System.Threading.Thread.Sleep(2000);
+            // textBox1.Text = Value[0].ToString();
+
+            //textBox2.Text = Value[0].ToString();
+            //textBox3.Text = Value[3].ToString();
+            timer1.Start(); // set timer1.Enabled = true
+                            // timer1.Enabled = true;
+
+
+        }
+
+        private void Exel()
+        {
+            xla = new Microsoft.Office.Interop.Excel.Application();
+            xla.Visible = true;
+            wb = xla.Workbooks.Add(Microsoft.Office.Interop.Excel.XlSheetType.xlWorksheet);
+            ws = (Microsoft.Office.Interop.Excel.Worksheet)xla.ActiveSheet;
+            range = (Microsoft.Office.Interop.Excel.Range)ws.get_Range("A1", "D1");
+            ws.Cells[1, 1] = "Thoi gian";
+            ws.Cells[1, 2] = " L1 Voltage";
+            ws.Cells[1, 3] = " L2 Voltage";
+            ws.Cells[1, 4] = " L3 Voltage";
+            range.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+            range.EntireColumn.AutoFit();
+            range.Font.Bold = true;
+            ws.Columns.AutoFit();
+            wb.SaveAs("C:\\Users\\os\\Desktop\\New folder (3)\\test.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Exel();
+        }
+        private void SavetoExel()
+        {
+
+            ws.Cells[i, 1] = DateTime.Now.ToString();
+            ws.Cells[i, 2] = Voltage1.ToString();
+            ws.Cells[i, 3] = Voltage2.ToString();
+            ws.Cells[i, 4] = Voltage3.ToString();
+
+            i++;
+        }
+        private void timer1_Tick(object sender, EventArgs e) // vẽ đồ thị theo khoảng thời gian Timer 1
+        {
+
+            point1.Add(new XDate(DateTime.Now), Voltage1);
+            point2.Add(new XDate(DateTime.Now), Voltage2);
+            point3.Add(new XDate(DateTime.Now), Voltage3);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            SavetoExel();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            xla.Visible = !xla.Visible;
+
+        }
+
+        private void zedGraphControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
